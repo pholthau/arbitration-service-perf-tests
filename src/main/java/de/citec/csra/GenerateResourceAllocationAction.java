118dd99c013216d877fb28841839096a9e836222
Patrick Holthaus <patrick.holthaus@uni-bielefeld.de>
2017-02-02 12:58:35 +0100

    add random allocation generator

---
+++ src/main/java/de/citec/csra/GenerateResourceAllocationAction.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.citec.csra;
+
+import java.util.Random;
+import java.util.UUID;
+import rsb.performance.test.actions.ActionSupport;
+import rsb.performance.test.parameters.ParameterSet;
+import rsb.performance.test.util.ExecutionException;
+import rsb.performance.test.util.SpecificationException;
+import rst.communicationpatterns.ResourceAllocationType.ResourceAllocation;
+import rst.communicationpatterns.ResourceAllocationType.ResourceAllocation.Initiator;
+import rst.communicationpatterns.ResourceAllocationType.ResourceAllocation.Policy;
+import rst.communicationpatterns.ResourceAllocationType.ResourceAllocation.Priority;
+import rst.timing.IntervalType.Interval;
+import rst.timing.TimestampType.Timestamp;
+
+/**
+ *
+ * @author pholthau
+ */
+public class GenerateResourceAllocationAction extends ActionSupport<Object> {
+
+
+	private static final Random RANDOM = new Random();
+	private static final int DELAY = 5000;
+	private static final int DURATION = 5000;
+	private static final String[] RESOURCES = {
+		"a", "b", "c", "d", "e", "f", "g", "h",
+		"i", "j", "k", "l", "m", "n", "o", "p",
+		"q", "r", "s", "t", "u", "v", "w", "x"
+	};
+
+	@Override
+	public Object execute(final ParameterSet parameters) throws	SpecificationException, ExecutionException, InterruptedException {
+		return ResourceAllocation.newBuilder().
+				setId(UUID.randomUUID().toString()).
+				setInitiator(Initiator.valueOf(RANDOM.nextInt(Initiator.values().length))).
+				setPolicy(Policy.valueOf(RANDOM.nextInt(Policy.values().length))).
+				setPriority(Priority.valueOf(RANDOM.nextInt(Priority.values().length))).
+				setSlot(
+					Interval.newBuilder().
+						setBegin(Timestamp.newBuilder().setTime(System.currentTimeMillis() + RANDOM.nextInt(DELAY))).
+						setEnd(Timestamp.newBuilder().setTime(System.currentTimeMillis() + RANDOM.nextInt(DURATION)))
+				).
+				addResourceIds(RESOURCES[RANDOM.nextInt(RESOURCES.length)]).
+				addResourceIds(RESOURCES[RANDOM.nextInt(RESOURCES.length)]).
+				addResourceIds(RESOURCES[RANDOM.nextInt(RESOURCES.length)]).
+				build();
+	}
+
+}
