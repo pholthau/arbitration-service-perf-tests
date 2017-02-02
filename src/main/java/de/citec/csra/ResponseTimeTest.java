d46f387586825ab66ee36d82142ea326aa9fc0b4
Patrick Holthaus <patrick.holthaus@uni-bielefeld.de>
2017-02-02 12:58:00 +0100

    use randomly generated allocations

--- src/main/java/de/citec/csra/ResponseTimeTest.java
+++ src/main/java/de/citec/csra/ResponseTimeTest.java
@@ -25,8 +25,6 @@
  */
 package de.citec.csra;
 
-import rsb.Event;
-
 import rsb.Factory;
 import rsb.Informer;
 import rsb.Scope;
@@ -36,16 +34,16 @@ import rsb.performance.test.RuntimeInformation;
 import rsb.performance.test.TestCase;
 import rsb.performance.test.TestPhase;
 import rsb.performance.test.actions.data.StaticData;
+import rsb.performance.test.actions.rsb.DynamicEvent;
 import rsb.performance.test.actions.rsb.InformerAction;
 import rsb.performance.test.actions.rsb.ParticipantValue;
+import rsb.performance.test.actions.rsb.ScopeValue;
 import rsb.performance.test.actions.timing.FixedRateTiming;
 import rsb.performance.test.parameters.Parameter;
 import rsb.performance.test.parameters.ParameterProduct;
 import rsb.performance.test.parameters.values.DoubleRange;
 import rsb.performance.test.repository.TestCasePlugin;
 import rst.communicationpatterns.ResourceAllocationType.ResourceAllocation;
-import rst.timing.IntervalType.Interval;
-import rst.timing.TimestampType;
 
 /**
  * @author jwienke
@@ -53,48 +51,41 @@ import rst.timing.TimestampType;
 @TestCasePlugin
 public final class ResponseTimeTest extends TestCase {
 
-    private static final Parameter<Double> FREQUENCY = new Parameter<>("frequency", Double.class);
+	private static final Parameter<Double> FREQUENCY = new Parameter<>("frequency", Double.class);
+	private final Informer<Object> informer;
 
-    private final Informer<Object> informer;
+	public ResponseTimeTest(final String name, final RuntimeInformation runtimeInformation) throws Exception {
+		super(name, runtimeInformation,
+				new ParameterProduct(new ParameterProduct.ParameterRange<Double>(FREQUENCY, new DoubleRange(1.0, 100.0, 10.0))));
 
-    private ResourceAllocation build() {
-        return ResourceAllocation.newBuilder().
-                setId("s").
-                setSlot(Interval.newBuilder().
-                        setBegin(TimestampType.Timestamp.newBuilder().setTime(10)).
-                        setEnd(TimestampType.Timestamp.newBuilder().setTime(15000))).
-                setInitiator(ResourceAllocation.Initiator.SYSTEM).
-                setPolicy(ResourceAllocation.Policy.MAXIMUM).
-                setPriority(ResourceAllocation.Priority.LOW).
-                setState(ResourceAllocation.State.REQUESTED).build();
-    }
+		DefaultConverterRepository.getDefaultConverterRepository().addConverter(new ProtocolBufferConverter<>(ResourceAllocation.getDefaultInstance()));
 
-    public ResponseTimeTest(final String name, final RuntimeInformation runtimeInformation) throws Exception {
-        super(name, runtimeInformation,
-                new ParameterProduct(new ParameterProduct.ParameterRange<Double>(FREQUENCY, new DoubleRange(1.0, 100.0, 10.0))));
+		this.informer = Factory.getInstance().createInformer("/coordination/allocation/");
+		this.informer.activate();
 
-        DefaultConverterRepository.getDefaultConverterRepository().addConverter(new ProtocolBufferConverter<>(ResourceAllocation.getDefaultInstance()));
-        
-        this.informer = Factory.getInstance().createInformer("/coordination/allocation/");
-        this.informer.activate();
-        
-        
+		final TestPhase firePhase = new TestPhase(
+				"randomrequestphase",
+				new FixedRateTiming(
+						new InformerAction(
+								new StaticData<>(new ParticipantValue<>(this.informer)),
+								new DynamicEvent(
+										new StaticData<>(new ScopeValue(new Scope("/coordination/allocation/"))),
+										StaticData.newClass(ResourceAllocation.class),
+										new GenerateResourceAllocationAction()
+								)
+						),
+						FREQUENCY,
+						StaticData.newNumber(5000L)
+				)
+		);
 
-        final TestPhase firePhase = new TestPhase("requestphase", new FixedRateTiming(
-                new InformerAction(new StaticData<Informer<?>>(new ParticipantValue<Informer<?>>(this.informer)),
-                        StaticData.newManualValue(new Event(new Scope("/coordination/allocation/"), ResourceAllocation.class,
-                                build()), 1)
-                ),
-                FREQUENCY, StaticData.newNumber(5000L))
-        );
+		this.addTestPhase(firePhase);
 
-        this.addTestPhase(firePhase);
+	}
 
-    }
-
-    @Override
-    public void cleanup() throws Exception {
-        this.informer.deactivate();
-    }
+	@Override
+	public void cleanup() throws Exception {
+		this.informer.deactivate();
+	}
 
 }
